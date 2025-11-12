166404bc8571ddf9d9e2e4ca03643b8b18670c09
Costi323 <cons.amadeus@gmail.com>
2025-11-12 17:37:11 +0100

    esto agrega el html y css

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,8 +1,13 @@
-# Shipping Cost Calculator
 
-## Input package weight and shipping rate
+
+
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
+
+ # Aquí hay una nueva actualización por <tu nombre de usuario de GitHub>
+  # estteeee>
+
+
 
 ## Calculate shipping cost
 shipping_cost = weight * rate
